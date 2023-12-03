fa21d5186317de482a45875ffc3b800f827cdd94
imadlourd <sirissani2002@gmail.com>
2023-12-03 15:02:50 +0100

    nothing

--- src/client/pages/shop/shop.jsx
+++ src/client/pages/shop/shop.jsx
@@ -3,47 +3,47 @@ import axios from "axios";
 import { Product } from "./product";
 import { CartContext } from "../context/cartContext";
 import "./shop.css";
-import {Navbar} from '../../components/navbar'
-import {Footer} from '../../components/footer'
+import { Navbar } from "../../components/navbar";
+import { Footer } from "../../components/footer";
 
 export const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const { addToCart } = useContext(CartContext);
+  const [products, setProducts] = useState([]);
+  const { addToCart } = useContext(CartContext);
 
-    useEffect(() => {
-        // Fetch data from  API
-        axios
-            .get("http://127.0.0.1:8000/api/parts")
-            .then((response) => {
-                setProducts(response.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching products:", error);
-            });
-    }, []);
+  useEffect(() => {
+    // Fetch data from  API
+    axios
+      .get("http://127.0.0.1:8000/api/parts")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
 
-    return (
-       <>
-       <Navbar/>
-        <div className="Shop">
-            <div className="shopTitle">
-                <h1>Wa Hassan Shop</h1>
-                <h2>Products</h2>
-            </div>
-            <div className="products">
-                {products.map((product) => (
-                    <Product
-                        key={product.id}
-                        id={product.id}
-                        name={product.name}
-                        price={product.sellPrice}
-                        pic={product.image}
-                        onAddToCart={() => addToCart(product)}
-                    />
-                ))}
-            </div>
+  return (
+    <>
+      <Navbar />
+      <div className="Shop">
+        <div className="shopTitle">
+          <h1>Wa Hassan Shop</h1>
+          <h2>Products</h2>
         </div>
-        <Footer/>
-       </>
-    );
+        <div className="products">
+          {products.map((product) => (
+            <Product
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.sellPrice}
+              pic={product.image}
+              onAddToCart={() => addToCart(product)}
+            />
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
